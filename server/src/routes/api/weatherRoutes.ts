bf1ceb49a27eb4b185f7e191a4a18c932592c0ce
Justin <j.hebenstreit25@gmail.com>
2024-08-09 21:58:21 -0500

    tried to fix errors after git pull

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -5,13 +5,18 @@ const router = Router();
 // import WeatherService from '../../service/weatherService.js';
 
 // TODO: POST Request with city name to retrieve weather data
+
 router.post('/', (req, res) => {
+
   // TODO: GET weather data from city name
+
   // TODO: save city to search history
+
 });
 
 // TODO: GET search history
-router.get('/history', async (req, res) => {});
+
+router.get('/history', async (req: Request, res:Response) => {});
 
 // * BONUS TODO: DELETE city from search history
 router.delete('/history/:id', async (req, res) => {});
