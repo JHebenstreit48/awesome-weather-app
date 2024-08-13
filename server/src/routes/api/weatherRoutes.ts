b9c96b54b1abe72a457135ffa2290329fab295d0
Justin <j.hebenstreit25@gmail.com>
2024-08-12 21:10:08 -0500

    continued working on TODOs.

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -2,17 +2,25 @@ import path from 'node:path';
 import { Router, type Request, type Response } from 'express';
 const router = Router();
 
+import historyService from '../../service/historyService';
+import weatherService from '../../service/weatherService';
+
+
 // TODO: POST Request with city name to retrieve weather data
 
 router.post('/', async(_req: Request, res: Response) => {
-  console.log(_req.body)
-  res.json()
-
+  try {
+    const weatherData = await weatherService.fetchLocationData();
+    res.json(weatherData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
   // TODO: GET weather data from city name
 router.get('/', async(_req: Request, res: Response) => {
-  res.sendFile(path.join(__dirname, '../../service/historyService.ts'))
+  res.sendFile(path.join(__dirname, '../../service/weatherService.ts'))
 });
 
 // TODO: save city to search history
